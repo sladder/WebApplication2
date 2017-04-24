74a9b3f7a8768c0e15cfacf209c3e6d455370d2b
Andre <andre_bmp@outlook.com>
2017-04-24 16:49:03 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication2/Controllers/InventarioController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class InventarioController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Inventario
+        public ActionResult Index()
+        {
+            return View(db.Inventarios.ToList());
+        }
+
+        // GET: Inventario/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventario inventario = db.Inventarios.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // GET: Inventario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventario/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InventarioID,Name,Description,Quantity,Red,Green,Blue")] Inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Inventarios.Add(inventario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(inventario);
+        }
+
+        // GET: Inventario/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventario inventario = db.Inventarios.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // POST: Inventario/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "InventarioID,Name,Description,Quantity,Red,Green,Blue")] Inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(inventario);
+        }
+
+        // GET: Inventario/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventario inventario = db.Inventarios.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // POST: Inventario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inventario inventario = db.Inventarios.Find(id);
+            db.Inventarios.Remove(inventario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/Models/Inventario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Inventario
+    {
+        public int InventarioID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public byte Red { get; set; }
+        public bool Green { get; set; }
+        public Boolean Blue { get; set; }
+    }
+}
